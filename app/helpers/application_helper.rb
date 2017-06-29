194ae8b10c23abadf9e10dec3a4675e85f19d28a
ivlin <ivlin112@gmail.com>
2017-06-29 18:19:50 -0400

    form displays - changed dependencies for foreign keys

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
 
   
+  def datum_value(datum, key) 
+    if datum.nil? || key.nil? || datum[key].nil?
+      nil
+    else
+      datum[key].to_s
+    end
+  end  
 
 end
