412c1976a0ee0ce3cc01b501d5ab4d5d3222ef88
ivlin <ivlin112@gmail.com>
2017-06-30 11:08:30 -0400

    fixed bug where form wasn't sending disabled values. removed redundant code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,20 @@
 module ApplicationHelper
 
   
-  def datum_value(datum, key) 
-    if datum.nil? || key.nil? || datum[key].nil?
-      nil
+  def datum_value(datum, attr_hash) 
+    if datum.nil? || attr_hash.nil? || datum[attr_hash.name].nil?
+    	nil
     else
-      datum[key].to_s
+      if attr_hash.type == :string || 
+      	attr_hash.type == :integer || 
+      	attr_hash.type == :float ||
+      	attr_hash.type == :date
+        datum[attr_hash.name].to_s
+      elsif attr_hash.type == :time
+      	datum[attr_hash.name].to_s(:time)
+      else
+      	datum[attr_hash.name]
+      end
     end
   end  
 
