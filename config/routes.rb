e5a28221ba65e0305c81c76c9616de8ff111db5a
ivlin <ivlin112@gmail.com>
2017-06-30 09:14:01 -0400

    set root to deliveries index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'deliveries#index'
+
   get 'deliveries', to: 'deliveries#index'  
   delete 'deliveries', to: 'deliveries#destroy'
 
