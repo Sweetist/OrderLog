678fa3f55360f55a74c3d6a9207f87ba4a827be8
ivlin <ivlin112@gmail.com>
2017-07-14 13:22:33 -0400

    misnamed a variable - fixed

--- db/migrate/20170627234749_create_orders.rb
+++ db/migrate/20170627234749_create_orders.rb
@@ -16,6 +16,6 @@ class CreateOrders < ActiveRecord::Migration[5.1]
       t.timestamps
     end
 
-    add_index :orders, :order, unique: true
+    add_index :orders, :number, unique: true
   end
 end
